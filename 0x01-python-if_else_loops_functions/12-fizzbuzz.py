b7881a06068e395435447c81a348e0cd0f7fe476
David-VargasV <4523@holbertonschool.com>
2022-05-03 17:52:11 -0700

    fizzbuzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -3,9 +3,9 @@ def fizzbuzz():
     for mul in range(1, 101):
         if mul % 3 == 0 and mul % 5 == 0:
         print("FizzBuzz"), end=" ")
-    elif mul % 5 == 0:
+        elif mul % 5 == 0:
         print("Buzz"), end=" ")
-    elif mul % 3 == 0:
+        elif mul % 3 == 0:
         print("Fizz"), end=" ")
-    else:
+        else:
         print(mul, end=" ")
