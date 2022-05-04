be3283437b1c1cdc5b1d9dce835828922fd67203
David-VargasV <4523@holbertonschool.com>
2022-05-03 17:38:47 -0700

    uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
-    for l in str:
-        letter = ord(l)
+    for char in str:
+        letter = ord(char)
         if letter > 96 and letter < 123:
             letter -= 32
         print(chr(letter), end='')
