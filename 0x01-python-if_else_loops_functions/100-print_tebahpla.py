c6d9c6a08a76b1c5940d719347fbbea696b97aec
David-VargasV <4523@holbertonschool.com>
2022-05-03 18:18:24 -0700

    print_tebahpla

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 for n in range(122, 96, -1):
-    if n % 2 != 0:
-        n -= 32
-    print("{}".format(chr(n)), end="")
+    num = n
+    if num % 2 != 0:
+        num -= 32
+    print("{}".format(chr(num)), end="")
