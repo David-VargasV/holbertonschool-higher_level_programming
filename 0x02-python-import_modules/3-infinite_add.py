ac035d9cd9791124ecbdc222edf3fdb9d342af50
David-vargasV <4523@holbertonschool.com>
2022-05-05 16:29:14 -0500

    infinite_add

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,9 +2,9 @@
 if __name__ == "__main__":
     from sys import argv
 
-count, add = 1, 0
+    larg = len(argv)
+    value = 0
 
-while count < len(argv):
-    add = (add + int(argv[count]))
-    count = count + 1
-print(add)
+    for num in range(1, larg):
+        value += int(argv[num])
+    print(value)
