267bbbfba6e54c5c6adce29b9b82c37f5f659fcf
David-vargasV <4523@holbertonschool.com>
2022-05-05 15:40:58 -0500

    infinite

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,3 +1,14 @@
 #!/usr/bin/python
+from itertools import count
+from unittest import result
+
+
 if __name__ == "__main__":
-    from sys import argv
+    import sys
+
+    count = len(sys.argv)
+    result = 0
+
+    for num in range(count - 1):
+        result += int(sys.argv[num + 1])
+        print("{}".format(result))
