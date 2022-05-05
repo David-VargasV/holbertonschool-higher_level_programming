759bacd3c2a367aec7eb7d05cc6edf4d43eb3ae7
David-vargasV <4523@holbertonschool.com>
2022-05-05 15:52:49 -0500

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
+    nlen = len(argv)
+    if nlen == 1:
+        print(f"{nlen - 1} arguments.")
+    elif nlen == 2:
+        print(f"{nlen - 1} argument:")
+    else:
+        print(f"{nlen - 1} arguments:")
+    for arguments in range(1, nlen):
+        print(f"{arguments}: {argv[arguments]}")
