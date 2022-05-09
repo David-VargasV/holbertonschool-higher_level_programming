68244ab32242400cc195620f265903cf94e9de5e
David-vargasV <4523@holbertonschool.com>
2022-05-09 14:49:32 -0500

    print_matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for x in matrix:
         for y in range(len(x)):
-            print(list[y], end="")
+            print((x[y]), end="")
             if y != len(x) - 1:
                 print(" ", end="")
-        print(" ")
+        print("")
