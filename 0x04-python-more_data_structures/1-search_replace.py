12e9e7aeeb2390bb9b2d923c3532be280b3cfd66
David-vargasV <4523@holbertonschool.com>
2022-05-10 12:02:19 -0500

    search_replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,2 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
+    n_list = my_list[:]
+    for x in range(len(n_list)):
+        if n_list[x] == search:
+            n_list[x] = replace
+    return n_list
