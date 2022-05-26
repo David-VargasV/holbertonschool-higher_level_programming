486b0c3b6b5db849cf2ba40d71d0890b260b7ba2
David-vargasV <4523@holbertonschool.com>
2022-05-26 13:42:19 -0500

    matrix_divided

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -4,3 +4,13 @@
 
 
 def matrix_divided(matrix, div):
+    if type(matrix) != list:
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    
+    
+    
+    
+    
+    type(matrix) != int and type(matrix) != float:
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    
