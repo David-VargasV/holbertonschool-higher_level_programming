f68fd7456942f1e87f1194268bac6fbbe4ead0c3
David-vargasV <4523@holbertonschool.com>
2022-05-26 13:43:54 -0500

    text

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -4,3 +4,20 @@
 
 
 def text_indentation(text):
+    if type(text) != str:
+        raise TypeError("text must be a string")
+
+    sign = 0
+    for x in text:
+        if sign == 0:
+            if x == " ":
+                continue
+            else:
+                sign = 1
+        if sign == 1:
+            if x == "." or x == "?" or x == ":":
+                print(x)
+                print()
+                sign = 0
+            else:
+                print(x, end="")
