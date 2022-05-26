b76b255ac424dab75bf26bf84e984b18c7319598
David-vargasV <4523@holbertonschool.com>
2022-05-26 13:43:33 -0500

    print_square

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -4,3 +4,9 @@
 
 
 def print_square(size):
+    if type(size) != int:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for x in range(size):
+        print("#" * size)
