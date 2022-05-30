81b59bd72a6599c25e92dc75b56ee987a882eb99
David-vargasV <4523@holbertonschool.com>
2022-05-30 16:25:35 -0500

    add_integer

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
+"""
+Integers addition
 
-"""Temporal"""
+Function that adds 2 integers
+"""
 
 
 def add_integer(a, b=98):
+
+    """Function that defines 2 integers and returns a result"""
+
     if type(a) != int and type(a) != float:
         raise TypeError("a must be an integer")
     if type(b) != int and type(b) != float:
