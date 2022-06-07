033e12ef1bee567a2a2be2bf0f3c8e2651fe4e00
David-vargasV <4523@holbertonschool.com>
2022-06-07 18:11:13 -0500

    inherits_from

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -9,4 +9,3 @@ of a class that inherited from the specified class.
 def inherits_from(obj, a_class):
     '''Function that returns True or False'''
     return isinstance(obj, a_class) and type(obj) is not a_class
-
