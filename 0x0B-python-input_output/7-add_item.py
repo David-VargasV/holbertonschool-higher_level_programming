f9fab6500e42800b94605af46ea95a8734ef4f4c
David-vargasV <4523@holbertonschool.com>
2022-06-08 18:19:49 -0500

    add_item

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,15 +6,15 @@ to a Python list, and then save them to a file
 '''
 
 
-import json
-from sys import argv
+import sys
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-filename = "add_item.json"
-with open(filename, 'a', encoding="utf-8") as f:
-    n_list = []
-    n_list.extend(argv[1:])
-    save_to_json_file(n_list, filename)
-    load_from_json_file(filename)
+try:
+    item = load_from_json_file("add_item.json")
+except:
+    item = []
+for x in range(1, len(sys.argv)):
+    item.append(sys.argv[x])
+save_to_json_file(item, "add_item.json")
