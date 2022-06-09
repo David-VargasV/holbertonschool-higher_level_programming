f0ed26115c229d70c160354692f86be16c8b3b96
David-vargasV <4523@holbertonschool.com>
2022-06-09 14:35:35 -0500

    my_int

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+'''Write a class MyInt that inherits from int'''
+
+
+class MyInt:
+    '''Initialize the class'''
+
+    def __eq__(self, value):
+        return self
+
+    def __ne__(self, value):
+        return self
