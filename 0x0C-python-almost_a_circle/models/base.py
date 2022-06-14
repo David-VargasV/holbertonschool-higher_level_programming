8cff29c3a71296a7a4d7e5aa567bb2883a2d7dbf
David-vargasV <4523@holbertonschool.com>
2022-06-14 15:04:45 -0500

    models

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -39,9 +39,10 @@ class Base:
     @staticmethod
     def from_json_string(json_string):
         '''returns the list of the JSON string representation json_string'''
-        if json_string is None or len(json_string) == 0:
+        if json_string is None or json_string == "[]":
             return []
-        return json.loads(json_string)
+        else:
+            return json.loads(json_string)
 
     @classmethod
     def create(cls, **dictionary):
