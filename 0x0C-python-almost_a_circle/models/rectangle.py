51142aa9d4a719f6891af7838c50f6de96d32724
David-vargasV <4523@holbertonschool.com>
2022-06-14 14:03:03 -0500

    models

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -107,8 +107,8 @@ class Rectangle(Base):
     def to_dictionary(self):
         '''returns the dictionary representation of a Rectangle'''
         return {"id": self.id,
-                "widht": self.width,
-                "height": self.height,
-                "x": self.x,
-                "y": self.y,
+                "widht": self.__width,
+                "height": self.__height,
+                "x": self.__x,
+                "y": self.__y,
                 }
