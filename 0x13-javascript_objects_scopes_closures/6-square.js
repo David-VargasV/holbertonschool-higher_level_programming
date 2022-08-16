63a27e452e515eea123c75dfe48017964daa0029
David-vargasV <4523@holbertonschool.com>
2022-08-16 16:26:01 -0500

    JavaScript_project2

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,10 +1,16 @@
 #!/usr/bin/node
-module.exports = class Square extends require('./5-square.js') {
+module.exports = class Square extends require('./4-rectangle.js') {
+  constructor (size) {
+    super(size, size);
+  }
+
   charPrint (c) {
     if (c === undefined) {
       this.print();
     } else {
-      for (let n = 0; n < this.height; n++) console.log(c.repeat(this.width));
+      for (let n = 0; n < this.height; n++) {
+        console.log(c.repeat(this.width));
+      }
     }
   }
 };
