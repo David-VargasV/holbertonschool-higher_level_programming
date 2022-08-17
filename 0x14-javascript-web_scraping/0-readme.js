f8bc57e64667c1ab6f40eeabf5d4da6cb3aa7a61
David-vargasV <4523@holbertonschool.com>
2022-08-17 14:48:53 -0500

    JavaScript

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 const fs = require('fs');
-const file = process.argv[2];
-fs.readFile(file, 'utf8', (err, data) => {
+fs.readFile(process.argv[2], 'utf8', (err, data) => {
   if (err) {
-    console.log(err);
-  } else {
-    console.log(data);
+    console.error(err);
+    return;
   }
+  console.log(data);
 });
