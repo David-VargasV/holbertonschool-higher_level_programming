c4a3bd2cb23f91abe96bf47fa079595ae8322cbb
David-vargasV <4523@holbertonschool.com>
2022-08-17 19:54:16 -0500

    JavScript

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,1 +1,17 @@
 #!/usr/bin/node
+const request = require('request');
+const ApiUrl = process.argv[2];
+request(ApiUrl, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  }
+  let n = 0;
+  for (const result of JSON.parse(body).results) {
+    for (const Wedge of result.characters) {
+      if (Wedge.includes(18)) {
+        n++;
+      }
+    }
+  }
+  console.log(n);
+});
--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,19 @@
 #!/usr/bin/node
+const request = require('request');
+const ApiUrl = process.argv[2];
+request(ApiUrl, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  }
+  const data = {};
+  for (const task of JSON.parse(body)) {
+    if (task.data === true) {
+      if (data[task.userId]) {
+        data[task.userId]++;
+      } else {
+        data[task.userId] = 1;
+      }
+    }
+  }
+  console.log(data);
+});
